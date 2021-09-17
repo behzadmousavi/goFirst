9a2521821adef25092c89531fb2005b42d6fe147
behzadmousavi <52397366+behzadmousavi@users.noreply.github.com>
2021-09-17 09:11:18 +0430

    added new routes

--- server.go
+++ server.go
@@ -12,17 +12,50 @@ func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", nil)
 	myRouter.HandleFunc("/add", addNumber)
+	myRouter.HandleFunc("/deduct", deductNumber)
+	myRouter.HandleFunc("/multiply", multiplyNumber)
+	myRouter.HandleFunc("/divide", divideNumber)
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func addNumber(w http.ResponseWriter, r *http.Request) {
-	first_number := r.FormValue("first")
-	second_number := r.FormValue("second")
-	first_number_int,_ := strconv.Atoi(first_number)
-	second_number_int,_ := strconv.Atoi(second_number)
+	firstNumber := r.FormValue("first")
+	secondNumber := r.FormValue("second")
+	firstNumberInt, _ := strconv.Atoi(firstNumber)
+	secondNumberInt, _ := strconv.Atoi(secondNumber)
 	var summation int
-	summation = first_number_int + second_number_int
+	summation = firstNumberInt + secondNumberInt
 	fmt.Fprintln(w, summation)
+}
+
+func deductNumber(w http.ResponseWriter, r *http.Request) {
+	firstNumber := r.FormValue("first")
+	secondNumber := r.FormValue("second")
+	firstNumberInt, _ := strconv.Atoi(firstNumber)
+	secondNumberInt, _ := strconv.Atoi(secondNumber)
+	var deduction int
+	deduction = firstNumberInt - secondNumberInt
+	fmt.Fprintln(w, deduction)
+}
+
+func multiplyNumber(w http.ResponseWriter, r *http.Request) {
+	firstNumber := r.FormValue("first")
+	secondNumber := r.FormValue("second")
+	firstNumberInt, _ := strconv.Atoi(firstNumber)
+	secondNumberInt, _ := strconv.Atoi(secondNumber)
+	var multiplication int
+	multiplication = firstNumberInt * secondNumberInt
+	fmt.Fprintln(w, multiplication)
+}
+
+func divideNumber(w http.ResponseWriter, r *http.Request) {
+	firstNumber := r.FormValue("first")
+	secondNumber := r.FormValue("second")
+	firstNumberInt, _ := strconv.Atoi(firstNumber)
+	secondNumberInt, _ := strconv.Atoi(secondNumber)
+	var division int
+	division = firstNumberInt / secondNumberInt
+	fmt.Fprintln(w, division)
 }
 
 func main() {
