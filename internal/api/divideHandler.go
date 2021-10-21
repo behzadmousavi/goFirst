33f59feffb6d3cb08d2679fc8c0701a1532def4a
Koorosh <Koorosh-m@users.noreply.github.com>
2021-10-21 19:36:21 +0330

    Boobs

--- internal/api/divideHandler.go
+++ internal/api/divideHandler.go
@@ -12,5 +12,10 @@ func DivideNumber(c echo.Context) error {
 	firstNumberFloat, _ := strconv.ParseFloat(firstNumber, 64)
 	secondNumberFloat, _ := strconv.ParseFloat(secondNumber, 64)
 	result := division(firstNumberFloat, secondNumberFloat)
+	if secondNumberFloat != 0 {
+		return c.JSON(http.StatusOK, result)
+	} else {
+		return c.JSON(http.StatusBadRequest, result)
+	}
 	return c.JSON(http.StatusOK, result)
 }
